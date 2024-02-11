6d1d8b9e25e99cc7f023edea810abbb483aad3ec
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-11 17:10:31 +0000

    Finished creating the list_all_directors method.  Working on show_dir_details method.

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,24 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
-end
+
+
+  def list_all_directors
+
+    @dir_list = Director.all
+
+
+    render({ :template => "layouts/directors"})
+
+  end # Of method.
+
+  def show_dir_details
+
+
+  end # Of method.
+  
+
+
+
+
+end # Of class
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "misc", :action => "list_all_directors" })
+
+  get("/directors/:director_id", { :controller => "misc", :action => "show_dir_details" })
 end
